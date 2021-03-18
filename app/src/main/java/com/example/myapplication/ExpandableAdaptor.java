ce2c775ea2680cb37928bce4cd505caa9e5d8a5f
ryantanyx <ryantanyx@gmail.com>
2021-03-18 13:04:00 +0800

    - finished comparison page

--- app/src/main/java/com/example/myapplication/ExpandableAdaptor.java
+++ app/src/main/java/com/example/myapplication/ExpandableAdaptor.java
@@ -16,11 +16,18 @@ public class ExpandableAdaptor extends BaseExpandableListAdapter {
     Context context;
     List<String> listGroup;
     HashMap<String, List<String>> listItem;
+    int size;
 
     public ExpandableAdaptor(Context context, List<String> listGroup, HashMap<String, List<String>> listItem){
         this.context = context;
         this.listGroup = listGroup;
         this.listItem = listItem;
+    }
+    public ExpandableAdaptor(Context context, List<String> listGroup, HashMap<String, List<String>> listItem, int size){
+        this.context = context;
+        this.listGroup = listGroup;
+        this.listItem = listItem;
+        this.size = 20;
     }
 
 
@@ -72,6 +79,19 @@ public class ExpandableAdaptor extends BaseExpandableListAdapter {
         return convertView;
     }
 
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent, int size) {
+        String group = (String) getGroup(groupPosition);
+        if (convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+
+        TextView textView = convertView.findViewById(R.id.list_parent);
+        textView.setText(group);
+        textView.setTextSize(size);
+        return convertView;
+    }
+
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         String child = (String) getChild(groupPosition,childPosition);
@@ -81,7 +101,21 @@ public class ExpandableAdaptor extends BaseExpandableListAdapter {
         }
 
         TextView textView = convertView.findViewById(R.id.list_child);
+        textView.setTextSize(16);
         textView.setText(child);
+        return convertView;
+    }
+
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent, int size) {
+        String child = (String) getChild(groupPosition,childPosition);
+        if (convertView == null){
+            LayoutInflater layoutInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView textView = convertView.findViewById(R.id.list_child);
+        textView.setText(child);
+        textView.setTextSize(size);
         return convertView;
 
     }
