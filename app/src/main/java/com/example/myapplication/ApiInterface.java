9d9f84d9f78fa14d459a16c932315d05a54e63a9
ryantanyx <ryantanyx@gmail.com>
2021-03-25 13:20:08 +0800

    -changed news api key location

--- app/src/main/java/com/example/myapplication/ApiInterface.java
+++ app/src/main/java/com/example/myapplication/ApiInterface.java
@@ -4,8 +4,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiInterface {
+    static final String NEWS_API_KEY = BuildConfig.NewsApiKey;
 
-    @GET("everything?q=(education OR student OR MOE OR PSLE) AND singapore&domains=straitstimes.com,bloomberg.com,channelnewsasia.com&sortBy=publishedAt&apiKey=6a5074d4afa44275a25bce2662fa7bad")
+    @GET("everything?q=(education OR student OR MOE OR PSLE) AND singapore&domains=straitstimes.com,bloomberg.com,channelnewsasia.com&sortBy=publishedAt&apiKey=" + NEWS_API_KEY)
     Call<News> getNews(
 
     );
