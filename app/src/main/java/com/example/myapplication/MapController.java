6acf7b22e399a8e3c07d63879feb63fe04441cef
ryantanyx <ryantanyx@gmail.com>
2021-03-26 10:24:43 +0800

    added current location marker cleaned up imports

--- app/src/main/java/com/example/myapplication/MapController.java
+++ app/src/main/java/com/example/myapplication/MapController.java
@@ -3,23 +3,11 @@ package com.example.myapplication;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
-import android.view.View;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.google.android.gms.maps.model.LatLng;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
